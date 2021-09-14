38c0e56abecedb1c962d01b67fbc507fe802af6c
Michael <michaelcaxias.dev@gmail.com>
2021-09-14 18:52:11 -0300

    Req 2: Ajustado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 // import { getUser } from '../services/userAPI';
+
 export default class Header extends Component {
   render() {
     return (
-      <header data-testid="header-component">
-        <h1>Header</h1>
-        <h2 data-testid="header-user-name">{() => getUser()}</h2>
-      </header>
+      <h1>Header</h1>
     );
   }
 }
--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class Loading extends Component {
   render() {
-    return <h1>Carregando..</h1>;
+    return <h1>Carregando...</h1>;
   }
 }
