00a30a8951a1d14de9c70746b0b678e34320f673
Michael <michaelcaxias.dev@gmail.com>
2021-09-14 20:27:21 -0300

    Req 5: Completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({ value });
+  }
+
   render() {
+    const { state: { value }, handleChange } = this;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <h1>Search</h1>
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ handleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ value.length < 2 }
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </>
     );
