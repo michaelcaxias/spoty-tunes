adfe24d99f4bb223f9bdb3920be189329957616a
Michael <michaelcaxias.dev@gmail.com>
2021-09-14 19:32:57 -0300

    Req 3: Completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
-// import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getUsername();
+  }
+
+  async getUsername() {
+    const userObject = await getUser();
+    const { name } = userObject;
+    this.setState({
+      username: name,
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading, username } = this.state;
     return (
-      <h1>Header</h1>
+      <>
+        <h1 data-testid="header-component">Header</h1>
+        <h2 data-testid="header-user-name">{ loading ? <Loading /> : username }</h2>
+      </>
     );
   }
 }
