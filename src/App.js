c7a57ec75231c6e2769b36b08b2394990cdd879e
Michael <michaelcaxias.dev@gmail.com>
2021-09-14 15:58:40 -0300

    Req 1: Completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
