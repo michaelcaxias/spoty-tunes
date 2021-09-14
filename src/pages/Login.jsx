1eb26c755213f6c584b5b9706e78d1b3771a33de
Michael <michaelcaxias.dev@gmail.com>
2021-09-14 17:31:14 -0300

    Req 2: 3/4 Completos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      savedName: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({
+      name: value,
+    });
+  }
+
+  loginSubmit = () => {
+    createUser(this.state);
+    this.setState({
+      savedName: true,
+    });
+  }
+
   render() {
+    const THREE = 3;
+    const { state: { name, savedName }, handleChange, loginSubmit } = this;
+    if (savedName) {
+      return (
+        <>
+          <Loading />
+          <Redirect to="/search" />
+        </>
+      );
+    }
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <form>
+          <input
+            type="text"
+            value={ name }
+            onChange={ handleChange }
+            data-testid="login-name-input"
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            onClick={ loginSubmit }
+            disabled={ name.length < THREE }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
