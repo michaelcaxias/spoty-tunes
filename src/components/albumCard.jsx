643a808b26b8ea5b2836ad39775ae4e342113786
Michael <michaelcaxias.dev@gmail.com>
2021-09-15 16:09:55 -0300

    Req 6.2: Completo

---
+++ src/components/albumCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class albumCard extends Component {
+  render() {
+    const { artistId, artistName, collectionId,  } this.props;
+    return (
+      <div>
+        <img src="" alt="Imagem Album" />
+
+      </div>
+    );
+  }
+}
