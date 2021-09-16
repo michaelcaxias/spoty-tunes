8dd3b0bffb68571eec2a5f93288d91ec069f5082
Michael <michaelcaxias.dev@gmail.com>
2021-09-16 12:08:13 -0300

    Req 8: Completo

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 export default class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      checked: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  async handleChange({ target: { id, checked } }) {
+    this.setState({ loading: true });
+    if (checked) {
+      await addSong(id);
+      console.log('adiciona');
+      this.setState({
+        loading: false,
+        checked: true,
+      });
+    } else {
+      await removeSong(id);
+      console.log('remove');
+      this.setState({
+        loading: false,
+        checked: false,
+      });
+    }
+  }
+
   render() {
-    const { musics: { previewUrl, trackName, trackId } } = this.props;
-    return (
+    const { props:
+    { musics:
+    { previewUrl, trackName, trackId } },
+    state: { loading, checked },
+    handleChange } = this;
+    const musicDiv = (
       <>
         <p>{ trackName }</p>
         <audio data-testid="audio-component" src={ previewUrl } controls>
@@ -12,13 +46,18 @@ export default class MusicCard extends Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
-        <label htmlFor="favorite" data-testid={ `checkbox-music-${trackId}` }>
+        <label htmlFor={ trackId } data-testid={ `checkbox-music-${trackId}` }>
           <input
             type="checkbox"
-            id="favorite"
+            id={ trackId }
+            checked={ checked }
+            onChange={ handleChange }
           />
         </label>
       </>
+    );
+    return (
+      loading ? <Loading /> : musicDiv
     );
   }
 }
